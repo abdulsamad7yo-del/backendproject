8acdffd77e313aa1c3eae3cb351664e7255f928b
Abdul Samad <abdulsamad7yo@gmail.com>
2026-01-21 19:33:55 +0530

    Add controllers and routes for comments, likes, playlists, subscriptions, and videos: implement CRUD operations and middleware integration

--- src/app.js
+++ src/app.js
@@ -31,11 +31,27 @@ app.use(express.static("public"))
 // help to perform crud operation on cookies
 app.use(cookiesParser())
 
+// import routes
 import userRouter from "./routes/user.routes.js"
 
-// routes declaration
+import subscriptionRouter from "./routes/subscription.routes.js"
+import videoRouter from "./routes/video.routes.js"
+import commentRouter from "./routes/comment.routes.js"
+import likeRouter from "./routes/like.routes.js"
+import playlistRouter from "./routes/playlist.routes.js"
+import dashboardRouter from "./routes/dashboard.routes.js"
+
+// Routes declaration
 
 app.use("/api/v1/users",userRouter)
+
+//todo
+app.use("/api/v1/subscriptions", subscriptionRouter)
+app.use("/api/v1/videos", videoRouter)
+app.use("/api/v1/comments", commentRouter)
+app.use("/api/v1/likes", likeRouter)
+app.use("/api/v1/playlist", playlistRouter)
+app.use("/api/v1/dashboard", dashboardRouter)
 
 // http://localhost:4000/api/v1/users/register
 
