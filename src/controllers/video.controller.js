a9d39be6a654537835d6e8a8868e0c71a25da3b5
Abdul Samad <abdulsamad7yo@gmail.com>
2026-01-26 20:07:05 +0530

    Enhance video publishing and updating logic: improve thumbnail handling, add error responses for missing videoId, and ensure secure URLs for uploads

--- src/controllers/video.controller.js
+++ src/controllers/video.controller.js
@@ -40,8 +40,8 @@ const publishAVideo = asyncHandler(async (req, res) => {
 
         const publish = await Video.create(
             {
-                videoFile: videoUpload.url,
-                thumbnail: thumbnailUpload.url,
+                videoFile: videoUpload.url || videoUpload.secure_url,
+                thumbnail: thumbnailUpload.url || thumbnailUpload.secure_url ,
                 title,
                 description,
                 duration: videoDuration,
@@ -105,13 +105,67 @@ const updateVideo = asyncHandler(async (req, res) => {
     // 2. thumbnail url is taken using req.file.path 
     // 3. upload to cloudinary and get url 
     // 4. FindByIdAndUpdate
-    // Video.findByIdAndUpdate(videoId,{title,description,videoFile})
+    // Video.findByIdAndUpdate(videoId,{title,description,thumbnail})
+
+    const { title, description } = req.body;
+    const thumbnailPath = req.file?.path;
+
+    const video = await Video.findById(videoId);
+    if (!video) {
+        return res.status(404).json(
+            new ApiResponse(404, null, "Video not found")
+        );
+    }
+
+    if (thumbnailPath && thumbnailPath.length > 0) {
+        const thumbnail = await uploadOnCloudinary(thumbnailPath);
+        video.thumbnail = thumbnail.url || thumbnail.secure_url;
+    }
+
+    if (title && title.length > 0) {
+        video.title = title;
+    }
+
+    if (description && description.length > 0) {
+        video.description = description;
+    }
+
+    await video.save({ validateBeforeSave: false });
+
+    return res
+    .status(200)
+    .json(
+        new ApiResponse(200, video, "Updated Video Information")
+    );
+
+
 
 })
 
 const deleteVideo = asyncHandler(async (req, res) => {
     const { videoId } = req.params
     //TODO: delete video
+    if (!videoId) {
+        throw new ApiError(400, "No Video Id Given")
+    }
+    try {
+
+        const deletedVideo = await Video.findByIdAndDelete(videoId)
+
+        if (!deletedVideo) {
+            throw new ApiError(404, "Video not found");
+        }
+
+        return res
+            .status(200)
+            .json(
+                200,
+                deletedVideo,
+                "Video Deleted"
+            )
+    } catch (error) {
+        throw new ApiError(500, `Error:${error.message}`)
+    }
 })
 
 const togglePublishStatus = asyncHandler(async (req, res) => {
